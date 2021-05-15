08d051705568a2d0dd1abd5a735549a7c9d1d10b
ajhunyady <aj@fluvio.io>
2021-05-14 17:49:26 -0700

    updated menus

---
+++ static/js/navbar.js
@@ -0,0 +1,14 @@
+/*
+$(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop > 65) {
+        $('#navbar').addClass('fixed-top');
+        $('#content').css('marginTop', '75px');
+        $('#toc-area').css('top', '75px');
+    } else if (scrollTop <= 65) {
+        $('#navbar').removeClass('fixed-top');
+        $('#content').css('marginTop', '0');
+        $('#toc-area').css('marginTop', '0');
+    }
+});
+*/
