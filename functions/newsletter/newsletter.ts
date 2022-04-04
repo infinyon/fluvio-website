9ba2dfa70e6a0891d268ca4312ff8ac066661ace
A.J <aj@infinyon.com>
2022-04-04 14:04:29 -0700

    updated functions

--- functions/newsletter/newsletter.ts
+++ functions/newsletter/newsletter.ts
@@ -1,17 +1,23 @@
 import { Handler } from '@netlify/functions'
 import fetch from 'node-fetch'
+import querystring from 'querystring';
 
 const  BUTTONDOWN_API_KEY = process.env.BUTTONDOWN_API_KEY
 interface User {
   name: string;
   email: number;
-  botField: string;
+  message: string;
 }
 
 export const handler: Handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  
   try {
-    const data = JSON.parse(event.body).payload.data;
-    console.log(data);
+    const user:User = JSON.parse(JSON.stringify(querystring.parse(event.body)));
+
+    console.log(user);
     return {
         statusCode: 200,
         body: JSON.stringify({
