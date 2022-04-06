b409b99ff81d394b1ad841af8b12d132699d2686
A.J <aj@infinyon.com>
2022-04-05 20:18:15 -0700

    upgraded bootstrap, and added subscribe button

--- functions/newsletter/newsletter.ts
+++ functions/newsletter/newsletter.ts
@@ -2,11 +2,16 @@ import { Handler } from '@netlify/functions'
 import fetch from 'node-fetch'
 import querystring from 'querystring';
 
-const  BUTTONDOWN_API_KEY = process.env.BUTTONDOWN_API_KEY
+const BUTTONDOWN_API_KEY = process.env.BUTTONDOWN_API_KEY
+const BUTTONDOWN_API_URL = "https://api.buttondown.email/v1/subscribers"
+
 interface User {
   name: string;
-  email: number;
-  message: string;
+  email: string;
+}
+
+function isArray(obj: Object) {
+  return Object.prototype.toString.call(obj) === '[object Array]';
 }
 
 export const handler: Handler = async (event, context) => {
@@ -17,19 +22,28 @@ export const handler: Handler = async (event, context) => {
   try {
     const user:User = JSON.parse(JSON.stringify(querystring.parse(event.body)));
     console.log(user);
-    
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: `POST Success`
-        })
-    };
+
+    let response = await fetch(BUTTONDOWN_API_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: user.email, notes: user.name }),
+    });
+
+    let message = await response.json();
+    if (isArray(message)) {
+      message = message[0];
+    }
+
+    console.log(`Buttondown Response (${response.status}): ${JSON.stringify(message)}`)
+    return {statusCode: response.status, body: JSON.stringify(message)};
+
   } catch (err) {
-    console.error('error ocurred in processing ', event);
-    console.error(err);
-    return {
-      statusCode: 500,
-      body: err.toString()
-    };
+
+    console.log(`Error: ${err.toString()}`);
+    return { statusCode: 500, body: err.toString()};
+
   }
 }
