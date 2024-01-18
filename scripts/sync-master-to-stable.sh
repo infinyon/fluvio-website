a0a9fb043f57a9fce6da03d5afc4bb14751965c4
Alan Chen <alan@infinyon.com>
2024-01-18 14:43:40 -0800

    chore: sync-master-to-stable, add head branch

--- scripts/sync-master-to-stable.sh
+++ scripts/sync-master-to-stable.sh
@@ -7,9 +7,13 @@ git checkout stable
 git pull
 
 branch_date=$(date +"%Y-%m-%d-%H.%M")
-git checkout -b sync-to-stable--$branch_date
+branch_name="sync-to-stable--$branch_date"
+git checkout -b $branch_name
 git merge origin/master -s recursive -Xtheirs -m "Merge master to stable $branch_date"
 
-gh pr create -B stable --title "merge master to stable $branch_date" --body "script run $branch_date $0 "
+gh pr create \
+    -B stable \
+    -H $branch_name \
+    --title "merge master to stable $branch_date" --body "script run $branch_date $0 "
 git checkout master
 
